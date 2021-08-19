dbfeb0465363b84a932cbd8d22efbf9316f46493
Kishan Kumar <kishank@testvagrant.com>
2021-08-19 20:14:22 +0530

    apiAutomationAdd

---
+++ src/main/java/apiManage/apiAutomation/APIWeatherDetailsAutomation.java
@@ -0,0 +1,26 @@
+package apiManage.apiAutomation;
+
+import apiManage.apiResonse.GetWeatherDetailsResponse;
+import apiManage.apiResonse.ManageWeatherDetails;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
+public class APIWeatherDetailsAutomation {
+
+    public static void main(String[] args) throws IOException, ParseException {
+        ManageWeatherDetails manageWeatherDetails=new ManageWeatherDetails();
+        GetWeatherDetailsResponse getWeatherDetailsResponse=new GetWeatherDetailsResponse();
+
+        getWeatherDetailsResponse.setTemp(manageWeatherDetails.getTemp());
+        getWeatherDetailsResponse.setPressure(manageWeatherDetails.getPressure());
+        getWeatherDetailsResponse.setHumidity(manageWeatherDetails.getHumidity());
+        getWeatherDetailsResponse.setWind(manageWeatherDetails.getWind());
+
+        System.out.println("Temperature: "+getWeatherDetailsResponse.getTemp()+"°C");
+        System.out.println("Pressure: "+getWeatherDetailsResponse.getPressure()+" mbar");
+        System.out.println("Humidity: "+getWeatherDetailsResponse.getHumidity()+" %");
+        System.out.println("Wind Speed: "+getWeatherDetailsResponse.getWind()+" km/h");
+
+    }
+}
---
+++ src/main/java/apiManage/apiResonse/GetWeatherDetailsResponse.java
@@ -0,0 +1,41 @@
+package apiManage.apiResonse;
+
+public class GetWeatherDetailsResponse {
+    String temp;
+    String pressure;
+    String wind;
+    String humidity;
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
+    }
+
+    public String getWind() {
+        return wind;
+    }
+
+    public void setWind(String wind) {
+        this.wind = wind;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+}
